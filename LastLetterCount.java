add872871f386df7fb0a1a32db2e0cd5dd6df984
Abhishek Bhatia <abhikbhatia@gmail.com>
2020-09-16 15:35:25 -0700

    Update file
    Update file

--- LastLetterCount.java
+++ LastLetterCount.java
@@ -1,26 +1,26 @@
 public class LastLetterCount {
-public int endWithmOrn(String stng) 
-{
-  int l = stng.length();
-  int ctr = 0;
-  stng = stng.toLowerCase();
-  for (int i = 0; i < l; i++) 
-  {
-    if (stng.charAt(i) == 's' || stng.charAt(i) == 'y') 
-	{
-      if (i < l-1 && !Character.isLetter(stng.charAt(i+1)))
-        ctr++;
-      else if (i == l-1)
-        ctr++;
-    }
-  }
-  return ctr;
-}
-
-public static void main (String[] args)
+    public int lastletter(String stng) 
     {
-      LastLetterCount letter= new LastLetterCount();
-      String str1 =  "The quick brown fox jumps over the lazy dog!";
-      System.out.println("The number of words ends eith s or y is: "+letter.endWithmOrn(str1));
-	  }
-}
+      int l = stng.length();
+      int ctr = 0;
+      stng = stng.toLowerCase();
+      for (int i = 0; i < l; i++) 
+      {
+        if (stng.charAt(i) == 's' || stng.charAt(i) == 'y') 
+        {
+          if (i < l-1 && !Character.isLetter(stng.charAt(i+1)))
+            ctr++;
+          else if (i == l-1)
+            ctr++;
+        }
+      }
+      return ctr;
+    }
+    
+    public static void main (String[] args)
+        {
+          LastLetterCount letter= new LastLetterCount();
+          String str1 =  "The quick brown fox jumps over the lazy dog!";
+          System.out.println("The number of words ends eith s or y is: "+letter.lastletter(str1));
+          }
+    }
