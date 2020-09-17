4f0ffa0edf4629ec6495ef32dfdee5f6bcad1e07
Abhishek Bhatia <abhikbhatia@gmail.com>
2020-09-16 18:05:16 -0700

    Update code
    update code

--- LastLetterCount.java
+++ LastLetterCount.java
@@ -1,26 +1,43 @@
 public class LastLetterCount {
-    public int lastletter(String stng) 
-    {
-      int l = stng.length();
-      int ctr = 0;
-      stng = stng.toLowerCase();
-      for (int i = 0; i < l; i++) 
-      {
-        if (stng.charAt(i) == 's' || stng.charAt(i) == 'y') 
-        {
-          if (i < l-1 && !Character.isLetter(stng.charAt(i+1)))
-            ctr++;
-          else if (i == l-1)
-            ctr++;
+    private String _firstSWord;
+    private String _firstYWord;
+
+    LastLetterCount(String input) {
+        int count = countSY(input);
+        String newSentence = wordSwap(input);
+        System.out.println(newSentence);
+        System.out.println("The number of words ends with s or y is: " + count);
+    }
+
+    public int countSY(String input) {
+        String[] words = input.split("[\\W]");
+        int endsInS = 0;
+        int endsInY = 0;
+        for (String word : words) {
+            if (word.toLowerCase().charAt(word.length() - 1) == 's') {
+                endsInS++;
+                if (endsInS == 1) {
+                    _firstSWord = word;
+                }
+            } else if (word.toLowerCase().charAt(word.length() - 1) == 'y') {
+                endsInY++;
+                if (endsInY == 1) {
+                    _firstYWord = word;
+                }
+            }
         }
-      }
-      return ctr;
+        return endsInS + endsInY;
     }
-    
-    public static void main (String[] args)
-        {
-          LastLetterCount letter= new LastLetterCount();
-          String str1 =  "The quick brown fox jumps over the lazy dog!";
-          System.out.println("The number of words ends eith s or y is: "+letter.lastletter(str1));
-          }
+
+    public String wordSwap(String input) {
+        input = input.replaceAll("\\b" +_firstSWord+ "\\b", "tempWord");
+        input = input.replaceAll("\\b" +_firstYWord+ "\\b", _firstSWord);
+        input = input.replaceAll("tempWord", _firstYWord);
+        return input;
     }
+
+    public static void main(String[] args) {
+        String str1 = "The quick brown fox jumps over the lazy dog!";
+        new LastLetterCount(str1);
+    }
+}
